aef0a03e27b2d6a7cd7a0cd66f184a0f760e71d4
reazulhasan44 <reazshovon44@gmail.com>
2025-02-17 17:45:54 +0600

    view Model folder added

---
+++ ProjectVilla.Web/ViewModels/VillaNumberVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjectVilla.Domain.Entities;
+
+namespace ProjectVilla.Web.ViewModels
+{
+    public class VillaNumberVM
+    {
+        public VillaNumber? VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? VillaList { get; set; }
+    }
+}
