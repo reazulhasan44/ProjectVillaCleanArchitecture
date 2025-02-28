8bdba0a658632ee997b51241d04bfc81ce7408f4
reazulhasan44 <reazshovon44@gmail.com>
2025-02-28 21:11:19 +0600

    villa add update

--- ProjectVilla.Application/Services/Implementation/VillaService.cs
+++ ProjectVilla.Application/Services/Implementation/VillaService.cs
@@ -1,22 +1,42 @@
-﻿using ProjectVilla.Application.Services.Interface;
+﻿using ProjectVilla.Application.Common.Interfaces;
+using ProjectVilla.Application.Services.Interface;
 using ProjectVilla.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Internal;
 
 namespace ProjectVilla.Application.Services.Implementation
 {
     public class VillaService : IVillaService
     {
-        public VillaService()
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IHostingEnvironment _webHostEnvironment;
+        public VillaService(IUnitOfWork unitOfWork, IHostingEnvironment webHostEnvironment)
         {
-                
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public void CreateVilla(Villa villa)
         {
-            throw new NotImplementedException();
+            if (villa.Image != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
+                string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+                using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+                villa.ImageUrl = @"\images\VillaImage\" + fileName;
+
+            }
+            else
+            {
+                villa.ImageUrl = "https://placehold.co/600x400";
+            }
+
+            _unitOfWork.Villa.Add(villa);
+            _unitOfWork.Save();
         }
 
         public bool DeleteVilla(int id)
@@ -26,17 +46,35 @@ namespace ProjectVilla.Application.Services.Implementation
 
         public IEnumerable<Villa> GetAllVillas()
         {
-            throw new NotImplementedException();
+            return _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
         }
 
         public Villa GetVillaById(int id)
         {
-            throw new NotImplementedException();
+            return _unitOfWork.Villa.Get(u => u.Id == id, includeProperties: "VillaAmenity");
         }
 
         public void UpdateVilla(Villa villa)
         {
-            throw new NotImplementedException();
+            if (villa.Image != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
+                string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+                if (!string.IsNullOrEmpty(villa.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, villa.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+                using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+                villa.Image.CopyTo(fileStream);
+
+                villa.ImageUrl = @"\images\VillaImage\" + fileName;
+            }
+            _unitOfWork.Villa.Update(villa);
+            _unitOfWork.Save();
         }
     }
 }
--- ProjectVilla.Web/Controllers/VillaController.cs
+++ ProjectVilla.Web/Controllers/VillaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjectVilla.Application.Services.Interface;
+using ProjectVilla.Domain.Entities;
 
 namespace ProjectVilla.Web.Controllers
 {
@@ -14,6 +15,50 @@ namespace ProjectVilla.Web.Controllers
         {
             var villas = _villaService.GetAllVillas();
             return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _villaService.CreateVilla(obj);
+                TempData["success"] = "The villa has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            var obj = _villaService.GetVillaById(villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+
+                _villaService.UpdateVilla(obj);
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
         }
     }
 }
